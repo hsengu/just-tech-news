14614bf022bf02467f15598f6cabf80dd2dfbe14
Hok S. Uy <hoksuy@gmail.com>
2021-09-19 12:38:12 -0700

    created view for homepage

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
