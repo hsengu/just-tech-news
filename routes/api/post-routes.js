caf44be049c31b1bb24dc3bd50a848d72f41971b
Hok S. Uy <hoksuy@gmail.com>
2021-09-18 19:44:16 -0700

    create comment model and updated routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,19 +1,36 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require('../../config/connection');
 
 router.get('/', (req, res) => {
-    console.log('===================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
-        order: [['created_at', 'DESC']],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
             }
         ]
-    }).then(dbPostData => res.json(dbPostData))
+    })
+        .then(dbPostData => {
+            // pass a single post object into the homepage template
+            // console.log(dbPostData[0]);
+            res.json(dbPostData);
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -14,10 +14,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
         include: [
             {
                 model: Post,
                 attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            // include the Comment model here:
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
                 model: Post,
@@ -25,19 +37,7 @@ router.get('/:id', (req, res) => {
                 through: Vote,
                 as: 'voted_posts'
             }
-        ],
-        where: {
-            id: req.params.id
-        }
-    }).then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-        }
-        res.json(dbUserData);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+        ]
     });
 });
 
