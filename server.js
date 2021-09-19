56ba816b07548145139fe8e7ba22ea0062cc0ea5
Hok S. Uy <hoksuy@gmail.com>
2021-09-19 12:21:45 -0700

    fixed sequelize connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
